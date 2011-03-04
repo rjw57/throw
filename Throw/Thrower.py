0771d1e8c55cc1cf2061023b4ab1fe57d7a84211
Rich Wareham <rjw57@cam.ac.uk>
2011-03-04 18:49:03 +0000

    over-use of 'are you sure' is quite annoying

--- Throw/Thrower.py
+++ Throw/Thrower.py
@@ -15,7 +15,6 @@ class Thrower(object):
         if to is None or len(to) == 0:
             self._interface.new_section()
 
-        while to is None or len(to) == 0:
             to = [ ]
 
             # We need to get some recipients
@@ -25,31 +24,18 @@ class Thrower(object):
             
             I'm going to ask you for a list of recipients. I'll keep going
             until you stop giving me e-mail addresses by just pressing 'enter'
-            at the prompt.
-            
-            If you make a mistake, just enter a blank e-mail address since
-            I'll ask you in a moment if the list is correct.""")
+            at the prompt.""")
 
-            should_continue = True
-            while should_continue:
-                to.append(self._interface.input('E-mail address to send files to'))
-                should_continue = (len(to[-1]) > 0)
-            to = to[:-1]
+            while len(to) == 0:
+                should_continue = True
+                while should_continue:
+                    to.append(self._interface.input('E-mail address to send files to'))
+                    should_continue = (len(to[-1]) > 0)
+                to = to[:-1]
 
-            if len(to) == 0:
-                self._interface.error("You need to give me at least one recipient.")
-                continue
+                if len(to) == 0:
+                    self._interface.error("You need to give me at least one recipient.")
 
-            self._interface.message("""I'm going to send the files to the following email addresses:""")
-
-            self._interface.literal_message('\n'.join([" - " + x for x in to]))
-
-            self._interface.message("""I'll just check with you that you entered all
-            the email addresses correctly. If you haven't, I'll ask for them again.""")
-            
-            if not self._interface.input_boolean("Is this list of e-mail addresses right?"):
-                to = None
-                
         self._interface.new_section()
         self._interface.message("""
         I'm going to send your file by email but before I do that, I need to know
