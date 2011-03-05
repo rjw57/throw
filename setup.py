6aee050a9b1f3d632d9cf11d1bb05e6f072eb4d3
Rich Wareham <rjw57@cam.ac.uk>
2011-03-05 12:30:14 +0000

    add a simple setup.py for installing throw
    Add a very simple setup.py which can be used for installing throw.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = "throw",
+    version = "0.0.1",
+    packages = find_packages(),
+    scripts = ['throw'],
+
+    install_requires = ['argparse'],
+
+    # metadata for upload to PyPI
+    author = "Rich Wareham",
+    author_email = "rjw57@cam.ac.uk",
+    description = "Simply share files from the command line",
+    license = "Apache 2.0",
+    keywords = "file sharing throw email",
+    url = "http://github.com/rjw57/throw",
+)
