8e771d17f1a6b4052f3b431bee9b017758b6838b
Rich Wareham <rjw57@cam.ac.uk>
2011-03-05 11:49:31 +0000

    actually create the email message
    Create an email message ready for sending and print it out.

--- Throw/Thrower.py
+++ Throw/Thrower.py
@@ -1,4 +1,16 @@
 import logging
+import smtplib
+import os
+import mimetypes
+
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import Throw.TerminalInterface as TerminalInterface
 import Throw.Config as cfg
 
@@ -61,4 +73,70 @@ class Thrower(object):
             if self._interface.input_boolean('Rememeber these values'):
                 self._config.set('user', 'name', identity['name'])
                 self._config.set('user', 'email', identity['email'])
+
+        outer = MIMEMultipart()
+        outer['Subject'] = 'Files thrown at you'
+        outer['From'] = '%s <%s>' % (identity['name'], identity['email'])
+        outer['To'] = ', '.join(to)
+        outer.preamble = 'Here are some files for you'
+
+        def add_file_to_outer(path):
+            if not os.path.isfile(path):
+                return
+
+            # Guess the content type based on the file's extension.  Encoding
+            # will be ignored, although we should check for simple things like
+            # gzip'd or compressed files.
+            ctype, encoding = mimetypes.guess_type(path)
+            if ctype is None or encoding is not None:
+                # No guess could be made, or the file is encoded (compressed), so
+                # use a generic bag-of-bits type.
+                ctype = 'application/octet-stream'
+            maintype, subtype = ctype.split('/', 1)
+            #if maintype == 'text':
+            #    fp = open(path)
+            #    msg = MIMEText(fp.read(), _subtype=subtype)
+            #    fp.close()
+            if maintype == 'image':
+                fp = open(path, 'rb')
+                msg = MIMEImage(fp.read(), _subtype=subtype)
+                fp.close()
+            elif maintype == 'audio':
+                fp = open(path, 'rb')
+                msg = MIMEAudio(fp.read(), _subtype=subtype)
+                fp.close()
+            else:
+                fp = open(path, 'rb')
+                msg = MIMEBase(maintype, subtype)
+                msg.set_payload(fp.read())
+                fp.close()
+                # Encode the payload using Base64
+                encoders.encode_base64(msg)
+
+            # Set the filename parameter
+            msg.add_header('Content-Disposition', 'attachment',
+                    filename=os.path.basename(path))
+            outer.attach(msg)
+
+        def add_dir_to_outer(dirpath):
+            if not os.path.isdir(dirpath):
+                return
+
+            add_paths_to_outer(\
+                    [os.path.join(dirpath, x) for x in os.listdir(dirpath)])
+
+        def add_paths_to_outer(paths):
+            for path in paths:
+                if os.path.isfile(path):
+                    add_file_to_outer(path)
+                elif os.path.isdir(path):
+                    add_dir_to_outer(path)
+
+        add_paths_to_outer(paths)
+
+        print(outer.as_string())
+
+        #server = smtplib.SMTP_SSL('localhost')
+        #server.sendmail(msg['From'], to, msg.as_string())
+        #server.quit()
 
