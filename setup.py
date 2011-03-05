82e9342949cac925df2752937111e83cd396a092
Rich Wareham <rjw57@cam.ac.uk>
2011-03-05 16:33:57 +0000

    add trove classifiers

--- setup.py
+++ setup.py
@@ -17,4 +17,22 @@ setup(
     license = "Apache 2.0",
     keywords = "file sharing throw email",
     url = "http://github.com/rjw57/throw",
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Communications',
+        'Topic :: Communications :: Email',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Utilities',
+
+        # make sure to use :: Python *and* :: Python :: 3 so
+        # that pypi can list the package on the python 3 page
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ],
 )
