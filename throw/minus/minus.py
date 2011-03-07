4597fd3da35953d226e15063c4faebf763ad0d7e
Rich Wareham <rjw57@cam.ac.uk>
2011-03-07 20:56:03 +0000

    fix indentation in minus module

--- throw/minus/minus.py
+++ throw/minus/minus.py
@@ -27,10 +27,14 @@ class User(object):
 
     def SignIn(self):
         """Signs in the User.
-        User.password1 attribute must be set to the passsword before calling this method.
-        Otherwise, an Exception will be raised."""
+        User.password1 attribute must be set to the passsword before calling
+        this method.  Otherwise, an Exception will be raised.
+
+        """
         if not self.password1:
-            raise Exception("There is no password set. Set the User.password1 attribute to the password before calling this method.")
+            raise Exception("There is no password set. Set the "+
+                "User.password1 attribute to the password before calling "+
+                "this method.")
         else:
             url = "http://min.us/api/SignIn"
             params = {"username":self.username, "password1":self.password1}
@@ -45,7 +49,7 @@ class User(object):
                         urllib2.HTTPCookieProcessor(self.cj))
                 self.login_data = urllib.parse.urlencode(params)
             reponse = self.opener.open(url, self.login_data)
-            
+
             parsed = json.loads(reponse.readlines()[0])
             if parsed["success"] == False:
                 raise Exception("Sign in failed.")
@@ -70,27 +74,33 @@ class User(object):
 
         # TODO
         # Check if the API returns Unavailable for the editor_id
-        
+
 
         url = "http://min.us/api/MyGalleries.json"
 
         response = self.opener.open(url)
-        parsed = _parseResponse  
+        parsed = _parseResponse
 
         if returnObject == True:
             galleryList = []
             for gallery in parsed["galleries"]:
-                galleryList.append(Gallery(gallery["reader_id"], editor_id=gallery["editor_id"], \
-                                           name=gallery["name"], last_visit=gallery["last_visit"], \
-                                           item_count=gallery["item_count"], \
-                                           clicks=gallery["clicks"]))
+                galleryList.append(
+                        Gallery(gallery["reader_id"],
+                            editor_id=gallery["editor_id"],
+                            name=gallery["name"],
+                            last_visit=gallery["last_visit"],
+                            item_count=gallery["item_count"],
+                            clicks=gallery["clicks"]))
         elif returnObject == False:
             return parsed
 
     def Galleries(self):
-        """To be used in the future if other users' Galleries are available through the API.
-        
-        This method currently does nothing."""
+        """To be used in the future if other users' Galleries are available
+        through the API.
+
+        This method currently does nothing.
+
+        """
         pass
 
 class Gallery(object):
@@ -105,7 +115,7 @@ class Gallery(object):
         self.last_visit = last_visit
         self.item_count = item_count
         self.clicks = clicks
-        
+
         # items is a list of direct links to multiple items in the gallery.
         # Use GetItems() to update this attribute.
         self.items = None
@@ -122,14 +132,14 @@ class Gallery(object):
         self.items = [a[16:].split('.')[0] for a in response["ITEMS_GALLERY"]]
 
         return (self.name, self.items)
-       
-        
+
+
     def SaveGallery(self, name=None, items=None):
         """Use this to update the gallery name or change sort order.
         Specify which attribute (name or items or both) you want to change."""
 
         url = 'http://min.us/api/SaveGallery'
-        
+
         if not name:
             if not self.name:
                 name = self.GetItems()[0]
@@ -141,17 +151,17 @@ class Gallery(object):
                 items = self.GetItems()[1]
             elif self.items:
                 items = self.items
-        
+
         params = {"name": name, "id":self.editor_id, "items":items}
 
-        try:        
+        try:
             response = _dopost(url, params)
         except:
             pass
         else:
             self.name = name
             self.items = items
-                    
+
 
 
 
@@ -165,25 +175,32 @@ class Item(object):
 
 
 def CreateGallery():
-    """Creates a Gallery on the server. Returns a Gallery object with the editor_id and reader_id."""
+    """Creates a Gallery on the server. Returns a Gallery object with the
+    editor_id and reader_id.
+
+    """
     url = 'http://min.us/api/CreateGallery'
 
     response = _dopost(url)
 
     _editor_id = response["editor_id"]
     _reader_id = response["reader_id"]
-    
+
     return Gallery(_reader_id, editor_id=_editor_id)
-    
+
 
 def UploadItem(filename, gallery, desiredName=None):
     """filename is the full file location and name of the file
-    WARNING: If your desiredName doesn't have a proper file extension (SHOULD be the same as the filename)
-             it'll still upload, but you won't be able to download it or view it online.
-             You can edit the name later to add the extension, but be careful, because it seems like you
-             can't change the extension after that."""
+    WARNING: If your desiredName doesn't have a proper file extension (SHOULD
+    be the same as the filename) it'll still upload, but you won't be able to
+    download it or view it online. You can edit the name later to add the
+    extension, but be careful, because it seems like you can't change the
+    extension after that.
 
-    url = 'http://min.us/api/UploadItem?'   # Must have the ? because urlencode doesn't add that on itself
+    """
+
+    # Must have the ? because urlencode doesn't add that on itself
+    url = 'http://min.us/api/UploadItem?'
 
     if desiredName:
         name = desiredName
@@ -191,10 +208,10 @@ def UploadItem(filename, gallery, desiredName=None):
         name = filename
 
     params = {"editor_id":gallery.editor_id, "filename":name}
-    
+
     with open(filename, 'rb') as f:
         itemData = f.read()
-        
+
     response = _dopost(url, params=params, payload=itemData)
 
     _id = response["id"]
@@ -202,7 +219,7 @@ def UploadItem(filename, gallery, desiredName=None):
     _width = response["width"]
     _filesize = response["filesize"]
 
-    return Item(_id, height=_height, width=_width, filesize=_filesize)    
+    return Item(_id, height=_height, width=_width, filesize=_filesize)
 
 def _doget(url):
     try:
@@ -231,7 +248,7 @@ def _dopost(url, params=None, payload=None):
     except NameError:
         response = urllib.request.urlopen(url, payload)
 
-    return _parseResponse(response) 
+    return _parseResponse(response)
 
 def _parseResponse(response):
     # response.readlines() is a list of many parts of the json.
