a526ff643f157d3877579bfaf2a6f8c068cde641
Rich Wareham <rjw57@cam.ac.uk>
2011-03-07 14:18:10 +0000

    include the README.markdown file as the long description

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 
+import os
 import version
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "throw",
@@ -20,6 +28,7 @@ setup(
     license = "Apache 2.0",
     keywords = "file sharing throw email",
     url = "http://github.com/rjw57/throw",
+    long_description = read('README.markdown'),
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
